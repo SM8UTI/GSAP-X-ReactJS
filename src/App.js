b65684b54328c62b9249e7ea45bc7944eda04397
Smruti Ranjan Nayak <56423859+SM8UTI@users.noreply.github.com>
2023-08-24 12:27:36 +0530

    custom mouse move code added ( gsap )

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import { gsap } from "gsap";
+import { forwardRef, useEffect, useImperativeHandle, useRef } from "react";
+import { Link } from "react-router-dom";
 
-function App() {
+const Circle = forwardRef(({ size, delay }, ref) => {
+  const el = useRef();
+
+  useImperativeHandle(
+    ref,
+    () => {
+      return {
+        moveTo(x, y) {
+          gsap.to(el.current, { x, y, delay });
+        },
+      };
+    },
+    [delay]
+  );
+
+  return <div className={`circle ${size}`} ref={el}></div>;
+});
+
+const App = () => {
+  const circleRef = useRef([]);
+
+  circleRef.current = [];
+
+  useEffect(() => {
+    const { innerWidth, innerHeight } = window;
+    circleRef.current.forEach((ref) =>
+      ref.moveTo(innerWidth / 2, innerHeight / 2)
+    );
+
+    const moveOn = ({ clientX, clientY }) => {
+      circleRef.current.forEach((ref) => ref.moveTo(clientX, clientY));
+    };
+
+    window.addEventListener("pointermove", moveOn);
+
+    return () => {
+      window.removeEventListener("pointermove", moveOn);
+    };
+  }, []);
+
+  const addCircleRef = (ref) => {
+    if (ref) {
+      circleRef.current.push(ref);
+    }
+  };
+
+  useEffect(() => {
+    const allClass = Array.from(document.querySelectorAll("a"));
+
+    allClass?.forEach((elem) => {
+      elem.addEventListener("mousemove  ", () => {
+        gsap.to(elem, {
+          y: 200,
+        });
+      });
+    });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="App">
+      <h1>Hello</h1>
+      <Circle size="sm" ref={addCircleRef} delay={0} />
+      <Circle size="md" ref={addCircleRef} delay={0.1} />
+      <Circle size="lg" ref={addCircleRef} delay={0.2} />
     </div>
   );
-}
+};
 
 export default App;
